41687229d9d59a67d2f9243846a06de3e51ab315
François GRUCHALA <francois.gruchala@gmail.com>
2019-03-10 15:48:14 +0100

    :sparkles: change log level to playu with stackdriver

--- functions/math/functions/add.js
+++ functions/math/functions/add.js
@@ -3,7 +3,7 @@ const mathService = require('../business/services/MathService');
 module.exports = (request, response) => {
   const operand1 = Number(request.query.operand1);
   const operand2 = Number(request.query.operand2);
-  console.log(`add operands : ${operand1} & ${operand2}`);
+  console.error(`add operands : ${operand1} & ${operand2}`);
 
   if (Number.isNaN(operand1) || Number.isNaN(operand2)) {
     response.status(400).send({message: 'Missing or invalid operands in the request.'});
